ea7f37165b2248b496a6d51e88ca2de8895c06c3
brainsnap <124458563+brainsnap@users.noreply.github.com>
2023-07-19 14:06:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -67,3 +67,47 @@ handleColorSchemeChange();
 
 // Listen for changes in the color scheme preference
 window.addEventListener("DarkReaderToggle", handleColorSchemeChange);
+
+// JavaScript for the hamburger menu
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const menuBar = document.querySelector(".menu-bar");
+
+  hamburgerIcon.addEventListener("click", function () {
+    menuBar.classList.toggle("open"); // Toggle the 'open' class to show/hide the menu bar
+  });
+  // Close the menu when clicking outside the menu bar
+  document.addEventListener("click", function (event) {
+    const targetElement = event.target;
+    if (
+      !menuBar.contains(targetElement) &&
+      !hamburgerIcon.contains(targetElement)
+    ) {
+      menuBar.classList.remove("open");
+    }
+  });
+});
+
+// JavaScript for updating the paragraph content without page reload
+document.addEventListener("DOMContentLoaded", function () {
+  const dynamicParagraph = document.querySelector(".dynamic-paragraph");
+
+  // Function to update paragraph content based on screen width
+  function updateParagraphContent() {
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth >= 320 && screenWidth <= 576) {
+      dynamicParagraph.textContent =
+        "Introducing BrainSnap - your ultimate study companion! Access excellent resources, notes, and a user-friendly interface to maximize your academic potential. Join us now for the best study experience and achieve academic excellence!";
+    } else {
+      dynamicParagraph.textContent =
+        "Introducing BrainSnap - the ultimate study companion for students! With our platform, you can say goodbye to the stress of finding excellent resources and notes for your studies. We provide a one-stop-shop for all your academic needs, ensuring you have access to the best resources available. Our user-friendly interface is designed to help you study with ease and efficiency, allowing you to maximize your potential and achieve academic success. So why choose BrainSnap? Because we understand the importance of education and are committed to providing you with the best study experience possible. Join us now and take the first step towards academic excellence!";
+    }
+  }
+
+  // Call the function initially
+  updateParagraphContent();
+
+  // Add event listener for window resize to dynamically update the content
+  window.addEventListener("resize", updateParagraphContent);
+});
